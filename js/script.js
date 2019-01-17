a395725231ded180a7d6a298a4811c131c5aca69
Steve Strassmann <steve@strassmann.com>
2019-01-16 23:34:34 -0500

    updated registration page

--- js/script.js
+++ js/script.js
@@ -63,25 +63,25 @@ function get_values(){
 var MAX_CHILDREN = 4;
 
 function on_addChild(e){
-  var count = $('.row.child:visible').length;
+  var count = $('.form-row.child:visible').length;
   if (count < MAX_CHILDREN){
-    $('#names .row.child:nth-child('+ (count+2) +')').css('display','block');
+      $('#names .form-row.child:nth-child('+ (count+2) +')').show(); // ADD
   }
   updateUpDownButtons();
   return false;
 }
 
 function on_removeChild(e){
-  var count = $('.row.child:visible').length;
+  var count = $('.form-row.child:visible').length;
   if (count > 1){
-    $('#names .row.child:nth-child('+ (count+1) +')').css('display','none');
+      $('#names .form-row.child:nth-child('+ (count+1) +')').hide();
   }
   updateUpDownButtons();
   return false;
 }
 
 function updateUpDownButtons(){
-  var count = $('.row.child:visible').length;
+  var count = $('.form-row.child:visible').length;
   if (count == 1){
     $('button#removeChild').prop('disabled', true);
   } else {
@@ -112,7 +112,7 @@ var TableFactory = {
     var result = $('<div/>', {'class': 'form_group', 'id':'names'})
       .append(
 	  this.make_header(),
-	  this.make_row(1).css('display', 'block'),
+	  this.make_row(1).show(),
 	  this.make_row(2),
 	  this.make_row(3),
 	  this.make_row(4),
@@ -123,46 +123,46 @@ var TableFactory = {
 
   make_one_header : function(col_class,name){
     return $('<div/>', {'class': col_class})
-      .append($('<p/>',{'class':'form-control-static'}).text(name));
+	  .append($('<b>').text(name));
     },
 
   make_header : function(){
-    return $('<div/>', {'class': 'row names_header'}).append(
-      this.make_one_header('col-md-4', 'Participating students'),
-      this.make_one_header('col-md-1', 'grade'),
-      this.make_one_header('col-md-2', 'phone'),
-      this.make_one_header('col-md-3', 'email (parent or child)'),
-      this.make_one_header('col-md-2', 'school')
+    return $('<div/>', {'class': 'mt-3 row names_header'}).append(
+      this.make_one_header('col-4', 'Participating students'),
+      this.make_one_header('col-1', 'grade'),
+      this.make_one_header('col-2', 'phone'),
+      this.make_one_header('col-3', 'email (parent or child)'),
+      this.make_one_header('col-2', 'school')
     );
   },
 
   // plus and minus buttons
   make_updown_buttons : function(){
     return  $('<div/>', {'class': 'row form-inline'}).append(
-      $('<div/>', {'class':'btn-group updown col-md-4'}).append(
-	$('<button/>', {'class':'btn btn-xs btn-default', 'aria-label':"Add child", 'id':'addChild'})
+      $('<div/>', {'class':'btn-group updown col-4'}).append(
+	  $('<button/>', {'type': 'button', 'class':'btn btn-xs', 'aria-label':"Add child", 'id':'addChild'})
 	  .append($('<span/>', {'class':''}).text('+')),
-	$('<button/>', {'class':'btn btn-xs btn-default', 'aria-label':"Remove child", 'id':'removeChild'})
+	  $('<button/>', {'type': 'button', 'class':'btn btn-xs', 'aria-label':"Remove child", 'id':'removeChild'})
 	  .append($('<span/>', {'class':''}).html('&ndash;')))
     );
   },
 
   make_row :function(N){
-    return $('<div/>', {'class': 'row child'})
-      .css('display','none')
+    return $('<div/>', {'class': 'form-row child'})
+	  .hide()
       .append(
-	this.make_input('col-md-4','name'+N, 'name'+N, 'text', 'Student ' + N + '\'s first and last name'),
-	this.make_select('col-md-1','grade'+N, 'grade'+N, this.grades),
-	this.make_input('col-md-2','phone'+N, 'phone'+N, 'tel', 'phone'),
-	this.make_input('col-md-3','email'+N, 'email'+N, 'email', 'email'),
-	this.make_select('col-md-2','school'+N, 'school'+N, this.schools)
+	this.make_input('col-4','name'+N, 'name'+N, 'text', 'Student ' + N + '\'s first and last name'),
+	this.make_select('col-1','grade'+N, 'grade'+N, this.grades),
+	this.make_input('col-2','phone'+N, 'phone'+N, 'tel', 'phone'),
+	this.make_input('col-3','email'+N, 'email'+N, 'email', 'email'),
+	this.make_select('col-2','school'+N, 'school'+N, this.schools)
       );
   },
 
   make_input : function(col_class, for_id, label, type, placeholder){
-    return $('<div/>', {'class': 'form_group ' + col_class})
+    return $('<div/>', {'class': col_class})
           .append(
-             $('<label/>', {'class': 'sr-only', 'for': for_id}).text(label),
+             //$('<label/>', {'class': 'sr-only', 'for': for_id}).text(label),
 	     $('<input/>', {'class': 'form-control',
 			    'type': type,
 			    'id':for_id,
